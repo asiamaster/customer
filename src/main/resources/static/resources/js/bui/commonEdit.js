6cfd09523ed8c50f36af94ae070780a02c633000
fanger <chenliangfang@diligrp.com>
2020-02-27 21:31:58 +0800

    feat: 客户-更新数据保存

--- src/main/resources/static/resources/js/bui/commonEdit.js
+++ src/main/resources/static/resources/js/bui/commonEdit.js
@@ -9,85 +9,10 @@
 /************  start **************/
 /************  end ***************/
 
-/************* 刷卡获取客户信息  start *****************/
-// 客户名称
-var customerNameAutoCompleteOption = {
-    width: 350,
-    serviceUrl: '/customer/list.action',
-    paramName: 'name',
-    displayFieldName: 'name',
-    transformResult: function (result) {
-        return {
-            suggestions: $.map(result, function (dataItem) {
-                return $.extend(dataItem, {
-                        value: dataItem.name + ' ' + dataItem.certificateNumber + ' ' + dataItem.cellphone
-                    }
-                );
-            })
-        }
-    },
-    selectFn: function (suggestion) {
-        $('#certificateNumber').val(suggestion.certificateNumber);
-        $('#_certificateNumber').val(suggestion.certificateNumber);
-        $('#customerCellphone').val(suggestion.cellphone);
-    }
-};
-// 证件号码
-var certificateNumberAutoCompleteOption = {
-    width: 350,
-    serviceUrl: '/customer/list.action',
-    paramName: 'certificateNumber',
-    displayFieldName: 'certificateNumber',
-    transformResult: function (result) {
-        return {
-            suggestions: $.map(result, function (dataItem) {
-                return $.extend(dataItem, {
-                        value: dataItem.name + ' ' + dataItem.certificateNumber + ' ' + dataItem.cellphone
-                    }
-                );
-            })
-        }
-    },
-    selectFn: function (suggestion) {
-        $('#customerName').val(suggestion.name);
-        $('#customerId').val(suggestion.id);
-        $('#customerCellphone').val(suggestion.cellphone);
-    }
-};
-$('#getCustomer').on('click', function (e) {
-    e.stopPropagation();
-    let user = reader();
-    $.ajax({
-        type: "POST",
-        url: "/customer/list.action",
-        // data: {certificateNumber : user.IDCardNo},
-        dataType: "json",
-        success: function (data) {
-            $('#customerName').val(data[0].name);
-            $('#customerId').val(data[0].id);
-            $('#certificateNumber').val(data[0].certificateNumber);
-            $('#_certificateNumber').val(data[0].certificateNumber);
-            $('#customerCellphone').val(data[0].cellphone);
-        },
-        error: function (a, b, c) {
-            bs4pop.alert('远程访问失败', {type: 'error'});
-        }
-    });
-});
-/**
- * 读身份证卡
- * @return {IDCardNo:'5116021989...'}
- * */
-function reader() {
-    if (!window.callbackObj) return;
-    return eval('(' + callbackObj.readIDCard() + ')');
-}
-/************* 刷卡获取客户信息  end *****************/
-
-
 
 /************ 初始化日期/时间 start **************/
-$(function () {
+laydateInt()
+function laydateInt() {
     lay('.laydate').each(function () {
         laydate.render({
             elem: this,
@@ -111,7 +36,7 @@ $(function () {
             }
         });
     });
-})
+}
 //始结束时间对比
 function isStartEndDatetime(date, el){
     let start = new Date($('.laystart').val());
@@ -129,8 +54,6 @@ function isStartEndDatetime(date, el){
 /************ 初始化日期/时间 end **************/
 
 
-
-
 /************ HTML反转义 start **************/
 function HTMLDecode(str) {
     var s = "";
@@ -144,5 +67,8 @@ function HTMLDecode(str) {
     s = s.replace(/<br\/>/g, "\n");
     return s;
 }
-
+//获取table Index
+function getIndex(str) {
+    return str.split('_')[1];
+}
 /************ HTML反转义 end **************/
--- src/main/resources/templates/htmltag/customer/updateJs.tag
+++ src/main/resources/templates/htmltag/customer/updateJs.tag
@@ -15,11 +15,11 @@
 
 
 
-    /******************************驱动执行区 begin***************************/
-
     $(function () {
         addCustomerItem();
+        laydateInt()
     });
+
 
     /**
      * 添加摊位
@@ -28,62 +28,42 @@
         $('#customerTable tbody').append(HTMLDecode(template('customerItem',{index:++itemIndex})))
     }
 
-
-    /******************************驱动执行区 end****************************/
-
-
-
-
-    /*****************************************函数区 begin************************************/
-
-    //HTML反转义
-    function HTMLDecode(str)
-    {
-        var s = "";
-        if (str.length == 0) return "";
-        s = str.replace(/&amp;/g, "&");
-        s = s.replace(/&lt;/g, "<");
-        s = s.replace(/&gt;/g, ">");
-        s = s.replace(/&nbsp;/g, " ");
-        s = s.replace(/&#39;/g, "\'");
-        s = s.replace(/&quot;/g, "\"");
-        s = s.replace(/<br\/>/g, "\n");
-        return s;
-    }
-
-    //获取table Index
-    function getIndex(str) {
-        return str.split('_')[1];
-    }
-
-    /*****************************************函数区 end**************************************/
-
-
-
-
-    /*****************************************自定义事件区 begin************************************/
-
     $('#addCustomer').on('click', function(){
         addCustomerItem();
-    })
+        laydateInt()
+    });
 
     //删除行事件 （删除摊位行）
     $(document).on('click', '.item-del', function () {
-        if ($('#stallTable tr').length > 2) {
+        if ($('#customerTable tr').length > 2) {
             $(this).closest('tr').remove();
         }
     });
 
     // 提交保存
     $('#formSubmit').on('click', function (e) {
-        if (!$('#saveForm').valid()) {
-            return false;
-        } else {
-            bui.loading.show('努力提交中，请稍候。。。');
-            let _formData = new FormData($('#saveForm')[0]);
-            $.ajax({
-                type: "POST",
-                url: "${contextPath}/customer/save.action",
+        // baseInfoForm
+        // cardInfoForm
+        // resourceInfoForm
+       if (!$('form').valid()) {
+           return false;
+       } else {
+           bui.loading.show('努力提交中，请稍候。。。');
+           let _formData = [];
+           let resourceArr = [];
+           let _baseAndCardData = [].concat($('#baseInfoForm').serializeArray(), $('#cardInfoForm').serializeArray());
+           let _resourceData = $('#resourceInfoForm').serializeArray();
+           $.each(_baseAndCardData, function(index, value){
+               _formData.push({[value.name]: value.value})
+           });
+           $.each(_resourceData, function(index, value){
+               resourceArr.push({[value.name]: value.value})
+           });
+           _formData.push({'resourceList': resourceArr});
+            console.log(_formData)
+           $.ajax({
+               type: "POST",
+               url: "${contextPath}/customer/save.action",
                 data: _formData,
                 processData: false,
                 contentType: false,
@@ -107,6 +87,7 @@
             });
         }
     });
+
 
     /*****************************************自定义事件区 end**************************************/
 </script>
